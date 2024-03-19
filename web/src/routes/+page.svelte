0b7aedbb0fffaf172317f2a084e64ba6b1d760db
kafi350 <kafiabdullahil96@gmail.com>
2024-03-19 11:52:08 +0100

    frontend

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -3,9 +3,7 @@
 </script>
 
 <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-12 w-12 text-blue-500 mb-8">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l9-5-9-5-9 5 9 5zm-12 12l12-7 12 7m-24 0a11.019 11.019 0 0112-11m12 11a11.019 11.019 0 01-12-11m12 0a11.019 11.019 0 0112 11m-12-11a11.019 11.019 0 00-12 11"></path>
-    </svg>
+    <img src="/images/logo_fak_3.png" alt="Logo" class="h-12 w-50 mb-8">
     <h1 class="text-4xl font-bold text-gray-700 mb-4">Welcome to Audio Forensics Framework</h1>
     <p class="text-center text-gray-600 mb-8">
         Audio Forensics Framework is a powerful tool for analyzing audio files. It uses advanced algorithms to segment audio files, generate waveform plots, classification of audio (emotion and gender) and more. 
